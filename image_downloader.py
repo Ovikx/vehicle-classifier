4a052624a22e175ae2cf58d5e3eee0cb3271434e
Ovik <47368700+Ovikx@users.noreply.github.com>
2021-12-28 20:31:41 -0500

    updated the image downloader

--- image_downloader.py
+++ image_downloader.py
@@ -13,17 +13,17 @@ class ImageDownloader:
         self.options = webdriver.ChromeOptions()
         self.options.add_argument('headless')
         self.driver = webdriver.Chrome(self.webdriver_path, options=self.options)
+        self.urls = set()
     
     async def download_task(self, url_data):
         async with aiohttp.ClientSession() as session:
-            print('Downloading images...')
             tasks = []
             for url, path, filename in url_data:
                 task = asyncio.ensure_future(self.retrieve_image(session, url, path, filename))
                 tasks.append(task)
 
             files = await asyncio.gather(*tasks)
-            for image_file, path, filename in files:
+            for image_file, path, filename, url in files:
                 try:
                     if image_file == None:
                         continue
@@ -35,26 +35,27 @@ class ImageDownloader:
                             image.save(f, 'PNG')
                 except Exception as e:
                     print(f'DOWNLOAD FAILED for {filename}- {e}')
-            print('Images downloaded')
+                    self.urls.remove(url)
     
     async def retrieve_image(self, session, url, path, filename):
         try:
             async with session.get(url, timeout=5) as response:
                 image_file = io.BytesIO(await response.content.read())
-                return (image_file, path, filename)
+                return (image_file, path, filename, url)
         except:
             print(f'TIMEOUT - {filename}')
-            return (None, None, None)
+            self.urls.remove(url)
+            return (None,)*4
 
     def download_images(self, query, limit, path):
         self.driver.get('https:images.google.com')
         search_bar = self.driver.find_element_by_name('q')
         search_bar.send_keys(query)
         search_bar.send_keys(Keys.RETURN)
-        urls = set()
+        self.urls = set()
         pb = ProgressBar(max_value=limit)
 
-        while len(urls) < limit:
+        while len(self.urls) < limit:
             thumbnails = self.driver.find_elements(By.CLASS_NAME, 'Q4LuWd')
             for thumbnail in thumbnails:
                 try:
@@ -64,18 +65,15 @@ class ImageDownloader:
                 expanded = self.driver.find_elements(By.CLASS_NAME, 'n3VNCb')
                 for image in expanded:
                     if image.get_attribute('src') and 'http' in image.get_attribute('src'):
-                        old_len = len(urls)
-                        urls.add(image.get_attribute('src'))
-                        print(f'Search progress: {pb.update(len(urls)-old_len)} {len(urls)}/{limit} done', end='\r')
-                if len(urls) >= limit:
+                        old_len = len(self.urls)
+                        self.urls.add(image.get_attribute('src'))
+                        if old_len != len(self.urls):
+                            asyncio.run(self.download_task([(image.get_attribute('src'), path, f'{query}{len(self.urls)}.jpg')]))
+                        print(f'Download progress: {pb.update(len(self.urls)-old_len)} {len(self.urls)}/{limit} done', end='\r')
+                if len(self.urls) >= limit:
                     break
 
         self.driver.quit()
 
-        url_data = [(url, path, f'{query}{i+1}.jpg') for i, url in enumerate(urls)]
-        asyncio.run(self.download_task(url_data))
-
-        return url_data
-
 downloader = ImageDownloader('C:\Program Files (x86)\\chromedriver.exe')
-downloader.download_images('toyota car', 50, 'images/car')
+downloader.download_images('mazda car', 500, 'images/mazda')
